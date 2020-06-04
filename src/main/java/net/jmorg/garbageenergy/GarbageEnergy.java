bb736bc0c58c3b309383bd82251eb267f7958d12
Ivan Gromov <vany.58.98.2013@gmail.com>
2020-06-04 17:11:50 +0300

    Bump version.

--- src/main/java/net/jmorg/garbageenergy/GarbageEnergy.java
+++ src/main/java/net/jmorg/garbageenergy/GarbageEnergy.java
@@ -33,9 +33,9 @@ public class GarbageEnergy extends BaseMod
     public static final String MODID = "GarbageEnergy";
     public static final String MODNAME = "Garbage Energy";
     public static final String MODGUIFACTORY = "net.jmorg.garbageenergy.common.gui.GuiFactory";
-    public static final String VERSION = "1.7.10R1.0.1B8";
+    public static final String VERSION = "1.7.10R1.1.0B1";
     public static final String DEPENDENCIES = "required-after:CoFHCore@[1.7.10R3.1.4,1.7.10R3.2.0);";
-    public static final String RELEASEURL = "https://raw.github.com/jm-organization/" + MODID + "/master/version.json";
+    public static final String RELEASEURL = "https://raw.github.com/gromovjm/" + MODID + "/master/version.txt";
     @Mod.Instance(MODID)
     public static GarbageEnergy instance;
     @SidedProxy(clientSide = "net.jmorg.garbageenergy.proxy.ClientProxy", serverSide = "net.jmorg.garbageenergy.proxy.ServerProxy")
